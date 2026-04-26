c25d60803e3f47154568d23b943dd05a81ad2d66
Harinath Reddy <harinathbliss@gmail.com>
2026-04-26 20:02:51 +0530

    k

--- mylambda/app.py
+++ mylambda/app.py
@@ -1,9 +1,12 @@
 import json
 from pydantic import BaseModel
 import logging
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel('INFO')
+
+bedrock = boto3.client(service_name="bedrock-runtime",region_name='us-east-1')
 
 
 class ChatDemo(BaseModel):
@@ -18,10 +21,34 @@ def lambda_handler(event,context):
      req = ChatDemo(**request_body)
 
      logging.info(f"Request Received {req}")
+    
+
+     native_request = {
+            "prompt": f"<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\n{req.message}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+            "max_gen_len": 512,
+            "temperature": 0.5,
+            "top_p": 0.9
+    }
+    
+     model_id = 'meta.llama3-8b-instruct-v1:0' 
+
+     response = bedrock.invoke_model(
+            modelId=model_id,
+            body=json.dumps(native_request)
+    )
+    
+     response_body = json.loads(response.get('body').read())
+        
+        
+     answer = response_body.get('generation', '')
+
+
+
      
      return {
           "statusCode":200,
            "body":json.dumps({
-                "message":req.message
+                "message":req.message,
+                "answer":answer
      })
      }
