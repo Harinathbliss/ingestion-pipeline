22db73ef5daf2d23758ac33b47a4827abcdc67fc
Harinath Reddy <harinathbliss@gmail.com>
2026-04-29 20:19:22 +0530

    pdf added

--- mylambda/app.py
+++ mylambda/app.py
@@ -5,6 +5,9 @@ import logging
 import boto3
 from uuid import uuid4
 import urllib.parse
+from pypdf import PdfReader
+import os
+import io
 
 logger = logging.getLogger()
 logger.setLevel('INFO')
@@ -17,15 +20,27 @@ def lambda_handler(event,context):
      
      bucket = event['Records'][0]['s3']['bucket']['name']
      key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+     file_name = key.split('.')
+     file_extension = file_name[-1]
+
+     
+     logger.info(bucket)
+     logger.info(key)
+     logger.info(file_extension)
+     
 
      response = s3_client.get_object(Bucket=bucket,Key=key)
      file_content = response['Body'].read().decode('utf-8')
+     pdf_reader = PdfReader(io.BytesIO(response['Body'].read()))
 
-     logger.info(file_content)
+     full_text = ""
+     for page in pdf_reader.pages:
+         text = page.extract_text()
+         if text:
+            full_text += text
+
      
-
-
-
+     logger.info(full_text)
      
      return {
           "statusCode":200,
