de2716f3164bf53b53e66a7848a185305fa7d801
Harinath Reddy <harinathbliss@gmail.com>
2026-05-05 20:18:07 +0530

    commit

--- mylambda/app.py
+++ mylambda/app.py
@@ -8,11 +8,18 @@ import urllib.parse
 from pypdf import PdfReader
 import os
 import io
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+
+
 
 logger = logging.getLogger()
 logger.setLevel('INFO')
 
 s3_client = boto3.client('s3')
+bedrock_client = boto3.client(service_name='bedrock-runtime')
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 
 def lambda_handler(event,context):
@@ -38,12 +45,32 @@ def lambda_handler(event,context):
          if text:
             full_text += text
 
+     splitter = RecursiveCharacterTextSplitter(
+               chunk_size=100,
+               chunk_overlap=20,
+               separators=["\n\n", "\n", " ", ""]
+     )
      
-     logger.info(full_text)
-     
+     chunks = splitter.split_text(text)
+     proceded_data = []
+     for i,chunk in enumerate(chunks):
+          native_request = {"input_chunk":chunk}
+          request = json.dumps(native_request)
+          bedrock_response = bedrock_client.invoke_model(
+          modelId="amazon.titan-embed-text-v1", 
+          contentType="application/json", 
+          accept="application/json", 
+          body=request
+               )
+          response_body = json.loads(bedrock_response.get('body').read())
+          embedding = response_body.get('embedding')
+          proceded_data.append({
+              "id":i,"vector":embedding,"text":chunk
+          })
+
      return {
           "statusCode":200,
            "body":json.dumps({
-            "message":"Successfully Downloaded File"
+            "message": proceded_data
      })
      }
